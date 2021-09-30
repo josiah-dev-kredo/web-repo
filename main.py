175b52375f8666770b075c067db275637ec543e3
myxroft <josiahbuhayan@gmail.com>
2021-09-30 09:45:02 +0800

    update on port access

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, request
 from flask.templating import render_template
+import os
 
 app = Flask(__name__)
 
@@ -12,5 +13,6 @@ def action():
     return "action"
 
 if __name__ == '__main__':
-   app.run(debug = True,host='0.0.0.0',port=33507)
+    port = int(os.getenv('PORT'))
+    app.run(debug = True,host='0.0.0.0',port=33507)
 
